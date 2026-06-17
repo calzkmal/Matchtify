5b91d9a1621e1daa8ee02776c2e31591e8d4f6a8
Calzy <caindyramdhani26@cjkt.idserve.net>
2026-06-17 14:13:24 +0700

    Initial Commit

---
+++ Matchtify/MatchtifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchtifyApp.swift
+//  Matchtify
+//
+//  Created by Calzy Akmal Indyramdhani on 17/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MatchtifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
